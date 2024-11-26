e7febc14488543116fee5258341d1794f6542761
Kyle Mistele <kyle@mistele.com>
2024-11-26 04:53:49 +0000

    feat: add functionary 3.1 with non-streaming parser

--- compose/configs/tool-parsers/llama3_xml.py
+++ compose/configs/tool-parsers/llama3_xml.py
@@ -1,4 +1,5 @@
 import json
+import re
 from typing import Any, Sequence, Tuple, Union
 from vllm.entrypoints.openai.protocol import (ChatCompletionRequest,
                                               DeltaFunctionCall, DeltaMessage,
@@ -13,10 +14,8 @@ from vllm.transformers_utils.tokenizer import AnyTokenizer
 logger = init_logger(__name__)
 
 
-@ToolParserManager.register_module("functionary_32")
+@ToolParserManager.register_module("functionary_31")
 class Functionary32ToolParser(ToolParser):
-
-    # TODO: tool call regex
 
     def __init__(self, tokenizer: AnyTokenizer):
         super().__init__(tokenizer)
@@ -26,11 +25,28 @@ class Functionary32ToolParser(ToolParser):
     def extract_tool_calls(
             self, model_output: str,
             request: ChatCompletionRequest) -> ExtractedToolCallInformation:
+
+        # if one or more function calls are present, handle them
+        matches = re.findall(r"<function=([^>]+)>(.*?)</function>", model_output)
+        if len(matches) == 0:
+            return ExtractedToolCallInformation(
+                tools_called=False,
+                tool_calls=[],
+                content=model_output
+            )
         return ExtractedToolCallInformation(
-            tools_called=False,
-            tool_calls=[],
-            content=model_output
+            tools_called=True,
+            tool_calls=[
+                ToolCall(
+                    type="function",
+                    function=FunctionCall(
+                        name=match[0],
+                        arguments=match[1]
+                    )
+                ) for match in matches
+            ]
         )
+
 
     def extract_tool_calls_streaming(
             self,
